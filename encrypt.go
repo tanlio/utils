a24c30095ab507f2546c66cb291d6805375e6900
掖掖 <chrysanthemumtanli@gmail.com>
2022-10-19 11:16:15 +0800

    rsa

--- encrypt.go
+++ encrypt.go
@@ -1,17 +1,19 @@
 package utils
 
 import (
+	"bytes"
 	"crypto/hmac"
 	"crypto/md5"
 	"crypto/sha1"
 	"crypto/sha256"
 	"crypto/sha512"
 	"encoding/hex"
+	"github.com/farmerx/gorsa"
 )
 
 //md5
 
-func Md5Str(str string) string {
+func MD5(str string) string {
 	m := md5.New()
 	m.Write([]byte(str))
 	return hex.EncodeToString(m.Sum(nil))
@@ -66,4 +68,63 @@ func HmacSHA512(key, data string) []byte {
 	h := hmac.New(sha512.New, keys)
 	h.Write([]byte(data))
 	return h.Sum(nil)
+}
+
+//Rsa私钥加密
+
+func RsaPriKeyEncrypt(plainText, key []byte) ([]byte, error) {
+	gorsa.RSA.SetPrivateKey(string(key))
+	var buffer bytes.Buffer
+	cryptTextTemp, err := gorsa.RSA.PriKeyENCTYPT(plainText)
+	if err != nil {
+		return nil, err
+	}
+	buffer.Write(cryptTextTemp)
+
+	return buffer.Bytes(), nil
+}
+
+//Rsa公钥解密
+
+func RsaPubKeyDecrypt(cryptText, key []byte) ([]byte, error) {
+	gorsa.RSA.SetPublicKey(string(key))
+	var buffer bytes.Buffer
+	plainTextTemp, err := gorsa.RSA.PubKeyDECRYPT(cryptText)
+	if err != nil {
+		return nil, err
+	}
+	buffer.Write(plainTextTemp)
+
+	return buffer.Bytes(), nil
+}
+
+//Rsa公钥加密
+
+func RsaPubKeyEncrypt(plainText, key []byte) ([]byte, error) {
+	gorsa.RSA.SetPublicKey(string(key))
+
+	var buffer bytes.Buffer
+	pubenctypt, err := gorsa.RSA.PubKeyENCTYPT(plainText)
+	if err != nil {
+		return nil, err
+	}
+
+	buffer.Write(pubenctypt)
+
+	return buffer.Bytes(), nil
+}
+
+//Rsa私钥解密
+
+func RsaPriKeyDecrypt(cryptText, key []byte) ([]byte, error) {
+	gorsa.RSA.SetPrivateKey(string(key))
+
+	var buffer bytes.Buffer
+	plainTextTemp, err := gorsa.RSA.PriKeyDECRYPT(cryptText)
+	if err != nil {
+		return nil, err
+	}
+	buffer.Write(plainTextTemp)
+
+	return buffer.Bytes(), nil
 }
