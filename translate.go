4a15fd51a3636f24b4a2633bca6a45ff0468d0d4
tanlio <chrysanthemumtanli@gmail.com>
2023-07-11 16:57:23 +0800

    xxx

--- translate.go
+++ translate.go
@@ -28,7 +28,7 @@ func TranslateLanguage(sourceLanguage, targetLanguage, text string, args ...inte
 	param["source_language"] = sourceLanguage
 	param["target_language"] = targetLanguage
 	param["version"] = version
-	response, err := GetRequest(uri, param, nil)
+	response, err := GetRequest(RequestMethodGet, uri, param, nil)
 	if err != nil {
 		fmt.Println("-----------TranslateLanguage", err)
 		return text
