7af379ac63ca378a5620123f3ec74f965d2244a7
chry <34668274+tanlio@users.noreply.github.com>
2025-12-09 16:25:37 +0800

    xxx

--- money.go
+++ money.go
@@ -1,32 +1,46 @@
 package utils
 
 import (
-	"reflect"
+	"fmt"
 
 	"github.com/shopspring/decimal"
 )
 
-func Fen2Yuan(price int64, args ...interface{}) string {
-	var num int32
-	if len(args) > 0 && reflect.TypeOf(args[0]).String() == "int" {
-		num = int32(args[0].(int))
-	} else {
-		num = 2
+func Fen2Yuan(price int64, args ...int32) string {
+	scale := int32(2)
+	if len(args) > 0 && args[0] >= 0 {
+		scale = args[0]
 	}
-	d := decimal.New(1, num)
-	result := decimal.NewFromFloat(float64(price)).DivRound(d, num).String()
-	return result
+
+	d := decimal.New(1, scale)
+	return decimal.NewFromInt(price).DivRound(d, scale).String()
 }
 
-func Yuan2Fen(price float64, args ...interface{}) int64 {
-	var num int32
-	if len(args) > 0 && reflect.TypeOf(args[0]).String() == "int" {
-		num = int32(args[0].(int))
-	} else {
-		num = 2
+func Yuan2Fen(price any, args ...int32) int64 {
+	scale := int32(2)
+	if len(args) > 0 && args[0] >= 0 {
+		scale = args[0]
 	}
-	d := decimal.New(1, num)
-	df := decimal.NewFromFloat(price).Mul(d)
 
-	return df.IntPart()
+	var priceStr string
+	switch v := price.(type) {
+	case string:
+		priceStr = v
+	case float32, float64:
+		priceStr = fmt.Sprintf("%v", v)
+	case int, int64, int32:
+		priceStr = fmt.Sprintf("%v", v)
+	default:
+		return 0
+	}
+
+	dPrice, err := decimal.NewFromString(priceStr)
+	if err != nil {
+		return 0
+	}
+
+	d := decimal.New(1, scale)
+	result := dPrice.Mul(d).Round(0)
+
+	return result.IntPart()
 }
